fbe89bd7d41d132a69599ba9110306d584815e25
Al-Amin931761 <alamin931761@gmail.com>
2022-04-17 02:06:06 +0600

    responsive services

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -5,7 +5,7 @@ const Service = ({ service }) => {
     const { name, price, picture, description } = service;
     return (
         <div className='service'>
-            <img width={300} src={picture} alt="" />
+            <img width={290} src={picture} alt="" />
             <h2>{name}</h2>
             <p>{price}</p>
             <p><small>{description}</small></p>

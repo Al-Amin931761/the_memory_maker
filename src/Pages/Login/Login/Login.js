165c06192e6ff1f6b4e19682e33ec4d17af0f729
Al-Amin931761 <alamin931761@gmail.com>
2022-04-17 23:48:17 +0600

    register part

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,23 +1,24 @@
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Login = () => {
 
+    const navigate = useNavigate();
     const emailRef = useRef('');
     const passwordRef = useRef('');
 
-    const handleSubmit = event => {
+    const handleLogin = event => {
         event.preventDefault();
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
         console.log(email, password);
     }
 
-
     return (
         <div className='w-50 mx-auto mt-5 mb-5'>
             <h2 className='text-center'>Please Login</h2>
-            <Form onSubmit={handleSubmit}>
+            <Form onSubmit={handleLogin}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control ref={emailRef} type="email" placeholder="Please Enter Your Email" />
@@ -28,13 +29,12 @@ const Login = () => {
                     <Form.Control ref={passwordRef} type="password" placeholder="Please Enter Your Password" />
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
+
                 <Button variant="primary" type="submit">
-                    Submit
+                    Login
                 </Button>
             </Form>
+            <p>New to The Memory Maker? <Link className='text-decoration-none' to='/register'><b>Please Register</b></Link></p>
         </div>
     );
 };
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Register = () => {
+
+    const navigate = useNavigate();
+    const nameRef = useRef('');
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+
+    const handleRegister = event => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        const name = nameRef.current.value;
+        console.log(name, email, password);
+    }
+
     return (
         <div>
-            <h2>Please Register</h2>
+            <div className='w-50 mx-auto mt-5 mb-5'>
+                <h2 className='text-center'>Please Register</h2>
+                <Form onSubmit={handleRegister}>
+                    <Form.Group className="mb-3" controlId="formBasicName">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control ref={nameRef} type="name" placeholder="Please Enter Your Name" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control ref={emailRef} type="email" placeholder="Please Enter Your Email" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control ref={passwordRef} type="password" placeholder="Please Enter Your Password" />
+                    </Form.Group>
+
+
+                    <Button variant="primary" type="submit">
+                        Register
+                    </Button>
+                </Form>
+                <p>Already have an account? <Link className='text-decoration-none' to='/login'><b>Please Login</b></Link>
+
+                </p>
+            </div>
         </div>
     );
 };

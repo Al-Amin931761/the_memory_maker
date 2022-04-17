6a4e1d7c5e438297d45cbb5c1392fcd5d07955c3
Al-Amin931761 <alamin931761@gmail.com>
2022-04-18 01:39:36 +0600

    google sign in

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,11 +1,15 @@
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
 import auth from '../../../firebase.init';
+import { FcGoogle } from 'react-icons/fc';
+import { IoMdLogIn } from 'react-icons/io';
 
 const Login = () => {
 
+
+    // signIn With Email And Password
     const [
         signInWithEmailAndPassword,
         user,
@@ -20,8 +24,18 @@ const Login = () => {
         event.preventDefault();
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
-        // console.log(email, password);
         signInWithEmailAndPassword(email, password);
+    }
+
+    // Sign In With Google 
+    const [signInWithGoogle, user2, loading2, error2] = useSignInWithGoogle(auth);
+
+    if (error2) {
+        return (
+            <div>
+                <p>Error: {error2.message}</p>
+            </div>
+        );
     }
 
     return (
@@ -35,15 +49,17 @@ const Login = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control ref={passwordRef} type="password" placeholder="Please Enter Your Password" />
+                    <Form.Control ref={passwordRef} type="password" placeholder="Please Enter Your Password " />
                 </Form.Group>
 
 
                 <Button variant="primary" type="submit">
-                    Login
+                    Login <IoMdLogIn className='fs-3' />
                 </Button>
             </Form>
             <p>New to The Memory Maker? <Link className='text-decoration-none' to='/register'><b>Please Register</b></Link></p>
+
+            <button onClick={() => signInWithGoogle()}> <FcGoogle className='fs-2' /> Google Sign In </button>
         </div>
     );
 };

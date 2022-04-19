1e5ae0d7c338026c032349919c18fe78a30640e7
Al-Amin931761 <alamin931761@gmail.com>
2022-04-19 15:32:07 +0600

    correct spelling

--- src/Pages/AboutMe/AboutMe.js
+++ src/Pages/AboutMe/AboutMe.js
@@ -7,7 +7,7 @@ const AboutMe = () => {
         <div className='about-container'>
             <h2 className='text-center m-3'>My name is Al-Amin.</h2>
             <div className='d-flex justify-content-center'>
-                <img width={390} src={myPhoto} alt="" />
+                <img width={360} src={myPhoto} alt="" />
             </div>
             <p className='fs-4 m-5 p-3' >Goals give our lives meaning. My goal is to become a web developer. Because Web development gives us the opportunity and the creative freedom to express ourselves on the world wide web.
                 <br />

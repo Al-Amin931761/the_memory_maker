fb0cb72b7185797cc0bd82ca97179064e534aabd
Al Amin <alamin931761@gmail.com>
2023-09-12 06:38:02 +0600

    update hero section

--- src/Pages/Cart/Checkout/CheckoutForm/CheckoutForm.js
+++ src/Pages/Cart/Checkout/CheckoutForm/CheckoutForm.js
@@ -7,6 +7,7 @@ import { signOut } from 'firebase/auth';
 import auth from '../../../../firebase.init';
 import { toast } from 'react-toastify';
 import './CheckoutForm.css';
+import { Button, Modal } from 'react-bootstrap';
 
 const CheckoutForm = () => {
     const stripe = useStripe();
@@ -17,6 +18,12 @@ const CheckoutForm = () => {
     const [clientSecret, setClientSecret] = useState("");
     const [processing, setProcessing] = useState(false);
     const [transactionId, setTransactionId] = useState('');
+    // checkbox 
+    const [agree, setAgree] = useState(false);
+    // terms and refund policy modal
+    const [showDeleteModal, setShowModal] = useState(false);
+    const ModalClose = () => setShowModal(false);
+    const ModalShow = () => setShowModal(true);
 
     // date and time 
     const today = new Date();
@@ -132,29 +139,86 @@ const CheckoutForm = () => {
     };
 
     return (
-        <form className='px-2 py-1 w-100' onSubmit={handleSubmit}>
-            <CardElement className='w-100'
-                options={{
-                    style: {
-                        base: {
-                            fontSize: '16px',
-                            color: '#424770',
-                            '::placeholder': {
-                                color: '#aab7c4',
+        <>
+            <form className='px-2 py-1 w-100' onSubmit={handleSubmit}>
+                <CardElement className='w-100'
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '16px',
+                                color: '#424770',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
                             },
                         },
-                        invalid: {
-                            color: '#9e2146',
-                        },
-                    },
-                }}
-            />
-            <button className='btn btn-outline-dark mt-2' type="submit" disabled={!stripe || !clientSecret || processing}>
-                Pay
-            </button>
-            {cardError && <p className='text-danger my-2'>{cardError}</p>}
-            {transactionId && <p className='text-primary my-2'>Transaction Id: {transactionId}</p>}
-        </form>
+                    }}
+                />
+                {/* terms and refund policy */}
+                <div className='my-3'>
+                    <input onClick={() => setAgree(!agree)} className="form-check-input" name='terms' id='terms' type="checkbox" />
+
+                    <label style={{ cursor: 'pointer' }} onClick={ModalShow} className={`ms-2 text-decoration-underline ${agree ? 'text-dark' : 'text-danger'}`} htmlFor="terms">Accept Terms of Use and Refund Policy</label>
+                </div>
+
+                <button className='btn btn-outline-dark' type="submit" disabled={!stripe || !clientSecret || processing || !agree}>
+                    Pay
+                </button>
+                {cardError && <p className='text-danger my-2'>{cardError}</p>}
+                {transactionId && <p className='text-primary my-2'>Transaction Id: {transactionId}</p>}
+            </form>
+
+
+            {/* terms and refund policy modal */}
+            <Modal show={showDeleteModal} onHide={ModalClose} backdrop="static" keyboard={false} size="lg">
+                <Modal.Header closeButton>
+                    <Modal.Title>Returns & Refunds Policy</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p>Thank you for shopping online at my website! If you are not fully satisfied with your purchase, I'm here to help.</p>
+
+                    <div>
+                        <h5>Returns</h5>
+                        <ul>
+                            <li>You have 7 days to return an item from the date you received it.</li>
+
+                            <li>To be eligible for a return:</li>
+                            <ul>
+                                <li>Your item must be unused and in the same condition as you had received it.</li>
+                                <li>Your item must be in the original packaging.</li>
+                                <li>You must provide digital photos (with original timestamp) on the condition of the item upon receipt.</li>
+                                <li>You must provide the receipt or proof of the purchase.</li>
+                            </ul>
+
+                            <li>I will separately provide you with our return mail address via email.</li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h5>Shipping Costs</h5>
+                        <p>You will be responsible for paying for the shipping costs for returning your item. The shipping costs you paid when you ordered the item are non-­refundable; and should the shipping costs be free when you ordered the item, the related shipping costs of such will be deducted from the refund (the receipt of such shipping costs shall be provided to you as proof).</p>
+                    </div>
+
+                    <div>
+                        <h5>Refunds</h5>
+                        <p>Once I receive your item, I will inspect it and notify you that I have received your returned item. I will immediately notify you on the status of your refund after inspecting the item. If your return is approved, I will initiate a refund to your credit card (or original method of payment). You will receive the credit within 14 days, depending on your card issuer's policies.</p>
+                    </div>
+
+                    <div>
+                        <h5>Contact Me</h5>
+                        <p>Should you have any questions on how to return your item to me, contact me any time:</p>
+                        <p>Email: <a href="mailto:alamin931761@gmail.com">alamin931761@gmail.com</a></p>
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="outline-dark" onClick={ModalClose}>Close</Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+
     );
 };
 
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import Banner from '../Banner/Banner';
 import Photography from '../Photography/Photography';
 import PageTitle from '../../Shared/PageTitle/PageTitle';
 import TravelDates from '../TravelDates/TravelDates';
+import HeroSection from '../HeroSection/HeroSection';
 
 const Home = () => {
     return (
         <div className='common-styles'>
             <PageTitle title="Home"></PageTitle>
-            <Banner></Banner>
+            <HeroSection></HeroSection>
             <div className='text-center fs-2 border border-2 border-primary'>
                 <p>I love capturing laughter, giggles, and lovely memories!​</p>
                 <p>I offer fashion, personal, outdoor, birthday, kids, newborn & babies, commercial, product, e-commerce, and event photography, if you want anything else let's chat and we can come up with something to fit what you need!</p>
             </div>
+
             <Photography></Photography>
             <TravelDates></TravelDates>
-        </div>
+        </div >
     );
 };
 
